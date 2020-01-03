17c5688078c80e2fa16ad290f0a56acecaf7b72f
nathan-chin <nathanchin@utexas.edu>
2020-01-03 01:02:19 -0600

    Lab Sh: working on file redir input
     * When writing to pipe, there seems to be a limit on how many chars you can
     *  write before you reach an infinite loop. ls writes too many chars to the
     *  pipe, so it fails and never stops looping. A more definite answer
     *  couldn't be found after debugging with GDB

--- os/user/nsh.c
+++ os/user/nsh.c
@@ -19,7 +19,7 @@ exec2pipe(int *p, char **args, int type, int n)
 
   // Execute with arguments
   if (type == REDIR_O) { // Output redirection
-    printf("OUTPUT");
+    //printf("OUTPUT");
     pipe(p);
     if (fork() == 0) {
       close(1);
@@ -36,7 +36,7 @@ exec2pipe(int *p, char **args, int type, int n)
       close(p[1]);
     }
   } else if (type == REDIR_I) { // Input redirection
-    printf("INPUT");
+    //printf("INPUT");
     char buf2[80]; // Hold line of input chars
     memset(buf2, '\0', sizeof buf2);
     fd = open(args[n], O_RDONLY); // Read file so each exec uses a line
@@ -88,7 +88,7 @@ exec2pipe(int *p, char **args, int type, int n)
       close(fd); // Close opened file
     }
   } else { // No redirection
-    printf("REDIR");
+    //printf("REDIR");
     pipe(p);
     if (fork() == 0) {
       close(p[0]); // Close read end so no leaking
@@ -97,7 +97,6 @@ exec2pipe(int *p, char **args, int type, int n)
       exec(args[0], args);
     } else {
       wait(0);
-      printf("Done execing\n");
       // Close write end of pipe so no leaking
       close(p[1]);
     }
@@ -107,7 +106,7 @@ exec2pipe(int *p, char **args, int type, int n)
 int
 main(void)
 {
-  static char buf[80]; // Hold line of input chars
+  char buf[80]; // Hold line of input chars
   char *line[MAXARG]; // Hold array of arg strings
 
   int prev; // Hold identification of previous read char
@@ -117,6 +116,8 @@ main(void)
   //int file[2]; // Holds index of file name. + if output, - if input
   int file;
   //int nFile; // Keep track of number of file
+  int p[2];
+  char execBuf[80];
 
   while (1) {
     prev = START;
@@ -130,7 +131,7 @@ main(void)
     gets(buf, sizeof buf); // Read in the input line into buf
     if (buf[0] == '\0') break;
     buf[strlen(buf) - 1] = '\0'; // Remove the newline character at the end
-    int special = 0;
+    int flag = START;
 
     //int p[2]; // Hold pipe fds
 
@@ -148,59 +149,91 @@ main(void)
           break;
         case '|':
           prev = PIPE;
-          special = 1;
+          flag = PIPE;
           break;
         case '<':
           prev = REDIR_I;
-          special = 1;
-          file = -n;
-          // Need to read in input file name before exec2pipe
+          flag = REDIR_I;
           break;
         case '>':
           prev = REDIR_O;
-          special = 1;
-          file = n;
-          int p[2];
-          memset(p, '\0', sizeof p);
-          printf("%d %d\n", p[0], p[1]);
-          printf("Exec with %d args\n", n);
-          exec2pipe(p, line, REDIR_O, n);
-          printf("%d %d\n", p[0], p[1]);
-          char buf3[80];
-          memset(buf3, '\0', sizeof buf3);
-          if (fork() == 0) {
-            close(0);
-            dup(p[0]);
-            close(p[0]);
-            close(p[1]);
-            printf("First read...\n");
-            gets(buf3, sizeof buf3);
-            buf3[strlen(buf3)-1] = '\0';
-            while (strlen(buf3) > 0) {
-              printf("pipe read: %s\n", buf3);
-              buf3[0] = '\0';
-              gets(buf3, sizeof buf3);
-              if (strlen(buf3) <= 0) break;
-              buf3[strlen(buf3)-1] = '\0';
-            }
-            exit(0);
-          } else {
-            wait(0);
-            close(p[0]);
-            close(p[1]);
-            printf("Done with pipe2exec\n");
-          }
+          flag = REDIR_O;
           break;
         default:
-          printf("Char here\n");
+          //printf("Char here\n");
           if (prev == CHAR) break;
           line[n++] = &buf[i];
           prev = CHAR;
+          if (flag == REDIR_O) {
+            fd = open(line[n-1], O_CREATE|O_WRONLY);
+            line[--n] = '\0'; // Don't include the file name in exec
+            memset(p, '\0', sizeof p);
+            //printf("%d %d\n", p[0], p[1]);
+            //printf("Exec with %d args\n", n);
+            exec2pipe(p, line, REDIR_O, n);
+            //printf("%d %d\n", p[0], p[1]);
+            memset(execBuf, '\0', sizeof execBuf);
+            if (fork() == 0) {
+              close(0);
+              dup(p[0]); // Read from pipe
+              close(1);
+              dup(fd); // Output to file
+              close(p[0]);
+              close(p[1]);
+              gets(execBuf, sizeof execBuf);
+              execBuf[strlen(execBuf)-1] = '\0';
+              while (strlen(execBuf) > 0) {
+                printf("%s\n", execBuf);
+                execBuf[0] = '\0';
+                gets(execBuf, sizeof execBuf);
+                if (strlen(execBuf) <= 0) break;
+                execBuf[strlen(execBuf)-1] = '\0';
+              }
+              exit(0);
+            } else {
+              wait(0);
+              close(fd);
+              close(p[0]);
+              close(p[1]);
+            }
+          } else if (flag == REDIR_I) {
+            fd = open(line[n-1], O_RDONLY);
+            line[--n] = '\0'; // Don't include the file name in exec
+            memset(p, '\0', sizeof p);
+            //printf("%d %d\n", p[0], p[1]);
+            //printf("Exec with %d args\n", n);
+            exec2pipe(p, line, REDIR_I, n);
+            //printf("%d %d\n", p[0], p[1]);
+            memset(execBuf, '\0', sizeof execBuf);
+            if (fork() == 0) {
+              close(0);
+              dup(p[0]); // Read from pipe
+              close(1);
+              dup(fd); // Output to file
+              close(p[0]);
+              close(p[1]);
+              gets(execBuf, sizeof execBuf);
+              execBuf[strlen(execBuf)-1] = '\0';
+              while (strlen(execBuf) > 0) {
+                printf("%s\n", execBuf);
+                execBuf[0] = '\0';
+                gets(execBuf, sizeof execBuf);
+                if (strlen(execBuf) <= 0) break;
+                execBuf[strlen(execBuf)-1] = '\0';
+              }
+              exit(0);
+            } else {
+              wait(0);
+              close(fd);
+              close(p[0]);
+              close(p[1]);
+            }
+          }
+        }
+        i++;
       }
-      i++;
-    }
 
-    if (prev <= NEWLINE && special == 0) {
+    if (prev <= NEWLINE && flag == START) {
       if (fork() == 0) {
         exec(line[0], line);
       } else {
